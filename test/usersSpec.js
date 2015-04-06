5abe4dd052cf33c8967dbf8ab92045aa0db15905
Andy Brown <asbrown002@gmail.com>
2015-04-06 10:41:04 -0700

    Add create endpoint to users resource

--- test/usersSpec.js
+++ test/usersSpec.js
@@ -1,8 +1,24 @@
 var should = require('chai').should();
 var users = require('../lib/truevault')('a-fake-key').users;
+var nock = require("nock");
 
 describe('users', function() {
   it('exists', function() {
     should.exist(users);
+  });
+
+  it('creates a user', function() {
+    nock('https://api.truevault.com').post('/v1/users')
+      .reply(200, {});
+
+    users.create({
+      username : 'username',
+      password : 'password'
+    }).then(function(value) {
+      should.exist(value);
+    }, function(err) {
+      //if we get here, this should fail
+      should.not.exist(err);
+    }).done();
   });
 });
